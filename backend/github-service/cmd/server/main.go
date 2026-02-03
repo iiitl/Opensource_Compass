9fd116f02e602c45bf0ddd0917f99a7f81137871
Kuber <kuberkhandare@gmail.com>
2026-02-03 19:11:23 +0530

    added search route with custom query for repo search

--- backend/github-service/cmd/server/main.go
+++ backend/github-service/cmd/server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github-service/internal/repos"
+	"github-service/routes"
 	"log"
 	"os"
 
@@ -18,37 +18,16 @@ func main(){
 	}
 
 	log.Println("Github Token Loaded")
-
-	// Temporary tester
-	repos, err := repos.FetchRepos(
-		[]string{"python", "go"},
-		nil,
-		[]string{"backend"},
-		token,
-	)
-	if err != nil{
-		log.Fatalf("Error fetching repos: %v", err)
-	}
-
-	log.Printf("Fetched %d repositories\n", len(repos))
-
-	if len(repos) > 0 {
-		log.Printf(
-			"🔎 Sample Repo → Name: %s | Stars: %d | URL: %s",
-			repos[0].Name,
-			repos[0].Stars,
-			repos[0].URL,
-		)
-	}
 	
 	r := gin.Default()
-
 
 	r.GET("/health", func(c *gin.Context){
 		c.JSON(200, gin.H{
 			"status": "github-service alive",
 		})
 	})
+
+	routes.RegisterGithubRoutes(r)
 
 	log.Println("Github service running on :8081")
 	r.Run(":8081")
--- backend/github-service/routes/github_routes.go
+++ backend/github-service/routes/github_routes.go
@@ -1,2 +1,52 @@
 package routes
 
+import (
+	"github-service/internal/repos"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterGithubRoutes(router *gin.Engine){
+	router.GET("/repos/search", func(c *gin.Context){
+		languagesParam := c.Query("languages")
+		frameworksParam := c.Query("frameworks")
+		domainsParam := c.Query("domains")
+
+		var languages []string
+		if languagesParam != ""{
+			languages = strings.Split(languagesParam, ",")
+		}
+
+		var frameworks []string
+		if frameworksParam != ""{
+			frameworks = strings.Split(frameworksParam, ",")
+		}
+
+		var domains []string
+		if domainsParam != ""{
+			domains = strings.Split(domainsParam, ",")
+		}
+
+		token := os.Getenv("GITHUB_TOKEN")
+		if token == ""{
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "GitHub token not configured",
+			})
+			return
+		}
+
+		repoList, err := repos.FetchRepos(languages, frameworks, domains, token)
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, repoList)
+	})
+
+}
